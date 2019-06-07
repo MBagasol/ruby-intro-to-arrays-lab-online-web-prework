7c27f46a3ad13d3860eaaba13b54d5eacd8c0ba2
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-07 04:50:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,19 +3,19 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  
+  [0,1]
 end
 
 def first_element
-
+  array[0]
 end
 
 def third_element
-  
+  array[2]
 end
 
 def last_element 
-  
+  array
 end
 
 def first_element_with_array_methods
