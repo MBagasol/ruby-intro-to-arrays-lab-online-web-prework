2e719762a6ee8035ddcc74e701ef1ab1f55b150b
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-07 04:51:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,15 +6,15 @@ def array_with_two_elements
   [0,1]
 end
 
-def first_element
+def first_element(array)
   array[0]
 end
 
-def third_element
+def third_element(array)
   array[2]
 end
 
-def last_element 
+def last_element(array)
   array[-1]
 end
 
